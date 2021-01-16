ef3f22fa290237fc6a7c4be9d11fcd345a21e9c3
Arseniy <arsen39@gmail.com>
2021-01-17 00:46:28 +0200

    Add Queue class and his methods

---
+++ index.js
@@ -0,0 +1,52 @@
+class Queue{
+  constructor() {
+    this._size = 0;
+  }
+
+  get isEmpty () {
+    return (!Boolean (this._size))
+  }
+
+  get size () {
+    return this._size;
+  }
+
+  enqueue (newElement) {
+    let counter = this._size++;
+    for (const item of this) {
+      this[counter] = this [--counter];
+    }
+    this[0] = newElement;
+    return this._size;
+  }
+
+  dequeue () {
+    const deleteElement = this[this._size - 1];
+    delete this[this._size-- - 1];
+    return deleteElement;
+  }
+
+  front () {
+    return this[this._size - 1];
+  }
+  
+  [Symbol.iterator](){
+    return new QueueIterator (this);
+  }
+}
+
+class QueueIterator {
+  constructor(queue) {
+    this.queue = queue;
+    this.currentValue = 0;
+  }
+
+  next() {
+    return {
+      value: this.queue[this.currentValue++],
+      done: this.currentValue > this.queue._size,
+    };
+  }
+}
+
+
